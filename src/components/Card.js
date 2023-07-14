85a38e5efe31ffbec06ba7379efc1da9e0670602
Visha1-S <76083887+V1sha1-S@users.noreply.github.com>
2023-07-14 09:53:12 +0530

    c2

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,19 @@ import './comp.css'
 import image from '../images/katie-zaferes.png'
 import image1 from '../images/star.png'
 
-const Card = () => {
+const Card = (props) => {
+  console.log(props)
   return (
     <section className='card'>
         <img src={image} className='card-image'/>
         <div className="card--stats">
             <img className="card-star"src={image1}/>
-            <span>5.0</span>
-            <span className='gray'>(6) • </span>
-            <span className='gray'>USA</span>
+            <span>{props.rating}</span>
+            <span className='gray'>({props.reviewCount})• </span>
+            <span className='gray'>{props.country}</span>
         </div>
-        <p>Life Lessons with Katie Zaferes</p>
-        <p><span className='bold'>From $136</span> / person</p>
+        <p>{props.title}</p>
+        <p><span className='bold'>From ${props.price}</span> / person</p>
     </section>
   )
 }
